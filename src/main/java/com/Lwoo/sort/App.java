ac86e8cbcff8ff669bbdd03adfc8113f2acf0b9a
Olinq <2629910752@qq.com>
2018-05-29 10:28:02 +0800

    quickstart

---
+++ src/main/java/com/Lwoo/sort/App.java
@@ -0,0 +1,13 @@
+package com.Lwoo.sort;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
